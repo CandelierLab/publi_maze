9c6138d09b4315b01689eded4f5ed1f3b4fe8cdf
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2026-02-05 09:26:49 +0100

    Misc modifications

--- Scripts/Parameter_space/parameter_space.py
+++ Scripts/Parameter_space/parameter_space.py
@@ -94,6 +94,7 @@ for run in range(1, n_runs):
       # Only the easy squares
       if eta>100 and dst<1: continue
       
+      
       print(f'\n─── {algo} eta={eta}, dst={dst} ─── run {run:04d}', '─'*10, datetime.datetime.now())
 
       # ─── Skip condition ──────────────────────
